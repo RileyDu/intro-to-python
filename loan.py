3eb9c96934d6a01b676914671bf900890894687e
Riley DuBord <thelifeofdu@gmail.com>
2024-04-30 16:04:31 -0500

    for loop for specific months created

--- loan.py
+++ loan.py
@@ -9,13 +9,19 @@ months = int(input('How many months do you want to see results for?\n')) # 24
 
 monthly_rate = apr/100/12
 
+for i in range(months):
 # Calculate interest to pay
-interest_paid = money_owed * monthly_rate
+    interest_paid = money_owed * monthly_rate
 
-# Add in interest
-money_owed = money_owed + interest_paid
+    # Add in interest
+    money_owed = money_owed + interest_paid
 
-# Make monthly payment
-money_owed = money_owed - payment
+    if (money_owed - payment < 0):
+        print('The last payment is', money_owed)
+        print('You paid off the loan in', i + 1, 'months')
+        break
 
-print('Paid', payment, 'of which', interest_paid, 'was interest', 'Now Owed', money_owed)
+    # Make monthly payment
+    money_owed = money_owed - payment
+
+    print('Paid', payment, 'of which', interest_paid, 'was interest', 'Now Owed', money_owed)
