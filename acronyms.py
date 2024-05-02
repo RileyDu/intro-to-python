8bc38ddc4306a48d258b8b3834e0199500081a79
Riley DuBord <thelifeofdu@gmail.com>
2024-05-02 16:14:35 -0500

    combined add and find functions

--- acronyms.py
+++ acronyms.py
@@ -1,13 +1,22 @@
-look_up = input("What software acronym would you like to look up?\n")
+def find_acronym():
+    look_up = input("What software acronym would you like to look up?\n")
 
-found = False
+    found = False
 
-with open('acronyms.txt') as file:
-    for line in file:
-        if look_up in line:
-            print(line)
-            found = True
-            break
+    with open('acronyms.txt') as file:
+        for line in file:
+            if look_up in line:
+                print(line)
+                found = True
+                break
+
+    if not found: 
+        print('The acronym was not found ')
+
+def add_acronym():
+    acronym = input('What acronym do you want to add?\n')
+    definition = input('What is the definition?\n')
+    with open('acronyms.txt', 'a') as file:
+        file.write(acronym + " - " + definition + '\n')
         
-if not found: 
-    print('The acronym was not found ')
+def main()
